9338d9341625d73d28b87cf9cd6027e619632958
Yifan <yifan@moguplatform.com>
2023-04-23 20:12:48 +0200

    Update sidebar

--- components/Sidebar.jsx
+++ components/Sidebar.jsx
@@ -1,8 +1,7 @@
 const Sidebar = ({ children }) => {
   return (
-    <div className="flex justify-between px-4 pt-4">
-      <h2>Dashboard</h2>
-      <h2>Welcome back</h2>
+    <div>
+      <main>{children}</main>
     </div>
   )
 }

a06d5dbf860dc081ab5c97ef8dc77b945becbe01
Yifan <yifan@moguplatform.com>
2023-04-29 14:00:22 +0200

    Add BarChart component

---
+++ components/RecentOrders.jsx
@@ -0,0 +1,5 @@
+const RecentOrders = () => {
+  return <div></div>
+}
+
+export default RecentOrders
--- components/TopCards.jsx
+++ components/TopCards.jsx
@@ -1,6 +1,6 @@
 const TopCards = () => {
   return (
-    <div className="grid log:grid-cols-5 gap-4 p-4">
+    <div className="grid lg:grid-cols-5 gap-4 p-4">
       <div className="lg:col-span-2 col-span-1 bg-white flex justify-between w-full border p-4 rounded-lg">
         <div className="flex flex-col w-full pb-4">
           <p className="text-2xl font-bold">$7,846</p>

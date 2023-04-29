e359e17052e52f3cdfabf61b5de998ef11c716a0
Yifan <yifan@moguplatform.com>
2023-04-29 17:26:11 +0200

    Fix order styling

--- components/RecentOrders.jsx
+++ components/RecentOrders.jsx
@@ -17,10 +17,10 @@ const RecentOrders = () => {
             <div className="pl-4">
               <p className="text-gray-800 font-bold">${order.total}</p>
               <p className="text-gray-400 text-sm">{order.name.first}</p>
-              <p className="lg:flex md:hidden absolute right-6 text-sm">
-                {order.date}
-              </p>
             </div>
+            <p className="lg:flex md:hidden absolute right-6 text-sm">
+              {order.date}
+            </p>
           </li>
         ))}
       </ul>

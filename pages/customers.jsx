ea1f20375831c33ae1d9e10acdb731b66375cf28
Yifan <yifan@moguplatform.com>
2023-04-29 17:23:40 +0200

    Add orders page

--- pages/customers.jsx
+++ pages/customers.jsx
@@ -4,7 +4,7 @@ import { data } from "../data/data.js"
 const customers = () => {
   return (
     <div className="bg-gray-100 min-h-screen">
-      <div className="flex justify-between p-4">
+      <div className="flex justify-between px-4 pt-4">
         <h2>Customers</h2>
         <h2>Welcome Back</h2>
       </div>
